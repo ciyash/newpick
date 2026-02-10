20eca304edb5b5efe176bb08dfdd0c59b6be0a37
ciyash <ciyashtechnologies@gmail.com>
2026-02-10 12:06:20 +0530

    dheer commit for sudheer push

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -2,6 +2,7 @@ import db from "../../config/db.js";
 import redis from "../../config/redis.js";
 import crypto from "crypto";
 import generateUserCode from "../../utils/usercode.js";
+import bcrypt from "bcrypt";
 
 /* =====================================================
    1️⃣ REQUEST SIGNUP OTP
