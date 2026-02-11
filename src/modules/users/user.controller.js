8988a65b4c5c23b172daecc34ea98fa22e9478fd
chandra8120 <chandrasekhar8120@gmail.com>
2026-02-11 13:27:42 +0530

    chandra changes

--- src/modules/users/user.controller.js
+++ src/modules/users/user.controller.js
@@ -1,6 +1,7 @@
 import { getUserProfileService, reduceMonthlyLimitService,  createFeedbackService,
   getMyFeedbacksService } from "./user.service.js";
 import { feedbackSchema } from "./user.validation.js";
+import db from "../../config/db.js";
 
 export const getUserProfile = async (req, res) => {
   try {
@@ -86,4 +87,103 @@ export const getMyFeedbacks = async (req, res) => {
 };
 
 
+export const pauseAccount = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const { duration } = req.body;
 
+    const durationMap = {
+      "15d": 15,
+      "1m": 30,
+      "3m": 90,
+      "6m": 180
+    };
+
+    if (!durationMap[duration]) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid duration (15d | 1m | 3m | 6m)"
+      });
+    }
+
+    const start = new Date();
+    const end = new Date();
+    end.setDate(end.getDate() + durationMap[duration]);
+
+    await db.query(
+      `UPDATE users SET
+        account_status = 'paused',
+        pause_start = ?,
+        pause_end = ?
+       WHERE id = ?`,
+      [start, end, userId]
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Account paused successfully",
+      pausedFrom: start,
+      pausedTill: end
+    });
+
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: err.message
+    });
+  }
+};
+
+export const resumeAccount = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    await db.query(
+      `UPDATE users SET
+        account_status = 'active',
+        pause_start = NULL,
+        pause_end = NULL
+       WHERE id = ?`,
+      [userId]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Account resumed successfully"
+    });
+
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      message: err.message
+    });
+  }
+};
+
+
+
+export const deleteAccount = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    await db.query(
+      `UPDATE users
+       SET account_status = 'deleted',
+           is_deleted = 1,
+           deleted_at = NOW()
+       WHERE id = ?`,
+      [userId]
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "Account deleted successfully"
+    });
+
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message
+    });
+  }
+};
--- src/modules/users/user.routes.js
+++ src/modules/users/user.routes.js
@@ -1,17 +1,23 @@
 import express from "express";
-import { getUserProfile,reduceMonthlyLimit,createFeedback,getMyFeedbacks } from "./user.controller.js";
-import { authenticate } from "../../middlewares/auth.middleware.js";
+import { getUserProfile,reduceMonthlyLimit,createFeedback,getMyFeedbacks ,  pauseAccount,
+  resumeAccount,deleteAccount} from "./user.controller.js";
+import { authenticate, checkAccountActive } from "../../middlewares/auth.middleware.js";
 import walletRoutes from "../wallet/wallet.routes.js";
 import subscriptionRoutes from "./subscription.routes.js";
 
 
 const app = express.Router();
 
-app.use("/userprofile", authenticate,getUserProfile);
-app.use("/wallet", authenticate,walletRoutes)
-app.use("/subscription", authenticate,subscriptionRoutes )
-app.use("/reduce-limit", authenticate,reduceMonthlyLimit);
-app.use("/create-feedback", authenticate, createFeedback);
-app.use("/get-feedback", authenticate, getMyFeedbacks);
+app.get("/userprofile", authenticate,getUserProfile);
+app.use("/wallet", authenticate,checkAccountActive,walletRoutes)
+app.use("/subscription", authenticate,checkAccountActive,subscriptionRoutes )
+app.use("/reduce-limit", authenticate,checkAccountActive,reduceMonthlyLimit);
+app.post("/create-feedback", authenticate,checkAccountActive, createFeedback);
+app.get("/get-feedback", authenticate, getMyFeedbacks);
+app.post("/pause-account", authenticate,checkAccountActive, pauseAccount);
+app.post("/resume-account", authenticate,resumeAccount);
+app.delete("/delete-account", authenticate,checkAccountActive, deleteAccount);
+
+
 
 export default app;
