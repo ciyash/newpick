d5ae1d744e35a6bbb290fd6b7d6e9b14a87dd4a5
chandra8120 <chandrasekhar8120@gmail.com>
2026-03-12 02:57:19 +0530

    kycss

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -193,7 +193,7 @@ export const signupService = async ({ mobile, otp }) => {
     const [result] = await conn.query(
       `INSERT INTO users
        (userid, usercode, name, email, mobile, region, address,
-        dob, referalid, nickname, category,
+        dob, referralid, nickname, category,
         email_verify, mobile_verify,
         created_at, age_verified)
        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0, 1, NOW(), 1)`,
