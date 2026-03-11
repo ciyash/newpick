9c727edaba8350ff76865f30165921d19f705aed
chandra8120 <chandrasekhar8120@gmail.com>
2026-03-11 10:17:43 +0530

    changes

--- src/modules/match/match.service.js
+++ src/modules/match/match.service.js
@@ -1,46 +1,5 @@
 import db from "../../config/db.js";
 
-// export const getMatchesByTypeService = async (type) => {
-
-//   let statusList = [];
-
-//   switch (type) {
-//     case "upcoming":
-//       statusList = ["UPCOMING"];
-//       break;
-
-//     case "live":
-//       statusList = ["LIVE"];
-//       break;
-
-//     case "completed":
-//       statusList = ["COMPLETED", "ABANDONED", "INREVIEW"];
-//       break;
-
-//     default:
-//       throw new Error("Invalid match type");
-//   }
-
-//   const [rows] = await db.query(
-//     `SELECT 
-//         id,
-//         series_id,
-//         seriesname,
-//         home_team_id,
-//         away_team_id,
-//         hometeamname,
-//         awayteamname,
-//         start_time,
-//         matchdate,
-//         status
-//      FROM matches
-//      WHERE status IN (?)
-//      ORDER BY start_time DESC`,
-//     [statusList]
-//   );
-
-//   return rows;
-// };
 
 export const getMatchesByTypeService = async (userId, type) => {
 
